b13caa48612995aeee3eb63b1cb48ddd8637c089
Hagmann Stephan <stephanhagmann@outlook.com>
2025-08-07 14:40:30 +0200

    fix: typo in classname

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,14 @@ import GameSelector from '@/components/GameSelector.vue';
 <template>
   <h1>Prosecco Party</h1>
   <LanguageSwitcher />
-  <GameSelector class="game-elemts"
+  <GameSelector class="game-elements"
     imagePath="images/schnapsidee.png"
     gamePath="/schnapsidee/game-config"
     gameName="Schnapsidee"
     i18nKey="home.schnapsidee-redirect" />
 </template>
 <style lang="scss">
-.game-elemts {
+.game-elements {
   display: flex;
   flex-direction: column;
   align-items: center;
